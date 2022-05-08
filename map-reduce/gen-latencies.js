d8230cc5b1be5f3c4b0c5e38f197e6e67a3de2c5
roylee0704 <roylee0704@gmail.com>
2022-05-08 08:04:24 +0800

    update

--- map-reduce/gen-latencies.js
+++ map-reduce/gen-latencies.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const HOST = process.env.HOST;
 const N = +process.env.N || 10000;
 const path = `./${HOST}/latencies.txt`;
-var stream = fs.createWriteStream(`./${HOST}/latencies.txt`, { flags: 'w' });
+var stream = fs.createWriteStream(`./${HOST}/latencies.txt`, { flags: "w" });
 
 console.log(`generating ${N} latencies to ${path}...`);
 
 const min = 5000;
 const max = 30000;
 [...Array(N)].forEach(function () {
-    const latency = min + Math.floor(Math.random() * (max - min));
+  const latency = min + Math.floor(Math.random() * (max - min));
 
-    stream.write(latency + "\n");
+  stream.write(latency + "\n");
 });
 stream.end();
